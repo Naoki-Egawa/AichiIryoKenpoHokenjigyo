758a4729b7b3483f18d47e2fb4c1c66b3f7dd779
Naoki Egawa <naokun0131@gmail.com>
2018-04-18 22:21:59 +0900

    commit test

--- AichiIryoKenpoHokenjigyo/MainWindow.xaml.cs
+++ AichiIryoKenpoHokenjigyo/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace AichiIryoKenpoHokenjigyo
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
